639560a1e8139ae0cb9566bd333135605873e9c1
Joorkar Romero <joorkarobrajham@gmail.com>
2021-11-02 17:24:57 -0600

    Maquetando con react

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import Layout from '../containers/Layout';
+import Login from '../containers/Login';
+import '../styles/global.css';
 
 const App = () => {
-  return <h1>Hola Mundo, Bienvenidos al Curso Practico de React!</h1>;
+  return (
+    <Layout>
+      <Login />
+    </Layout>
+  );
 };
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -28,6 +29,10 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(css|scss)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
     ],
   },
   plugins: [
@@ -35,5 +40,12 @@ module.exports = {
       template: './public/index.html',
       filename: './index.html',
     }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
   ],
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    port: 3005,
+  },
 };
