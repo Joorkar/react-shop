20e173f6ccf626102bbf117aacdb078fe86189fe
Joorkar Romero <joorkarobrajham@gmail.com>
2021-11-02 19:31:36 -0600

    Manejando rutas con react-router-dom

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/NotFound.scss';
+
+const NotFound = () => {
+  return (
+    <div className="container">
+      <img
+        className="image"
+        src="https://i.stack.imgur.com/6M513.png"
+        alt="Error 404"
+      />
+    </div>
+  );
+};
+
+export default NotFound;
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+    publicPath: '/',
   },
   mode: 'development',
   resolve: {
@@ -45,7 +46,8 @@ module.exports = {
     }),
   ],
   devServer: {
-    static: path.join(__dirname, 'dist'),
-    port: 3005,
+    /* static: path.join(__dirname, 'dist'),
+    port: 3005, */
+    historyApiFallback: true,
   },
 };
