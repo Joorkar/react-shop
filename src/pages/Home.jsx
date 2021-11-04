98fa92224e5b5ed96cd5097c19ed700e1f4006ea
Joorkar Romero <joorkarobrajham@gmail.com>
2021-11-03 19:09:16 -0600

    ADD componentes y rutas de React Shop

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from '../components/Header';
 
 const Home = () => {
   return (
-    <div>
-      <h1>React Shop</h1>
-    </div>
+    <>
+      <ProductList />
+    </>
   );
 };
 
--- src/pages/SendEmail.jsx
+++ src/pages/SendEmail.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import '../styles/RecoveryPassword.scss';
+import '../styles/SendEmail.scss';
 
-const RecoveryPassword = () => {
+const SendEmail = () => {
   return (
-    <div className="login">
+    <div className="SendEmail">
       <div className="form-container">
         <img src="./logos/logo_yard_sale.svg" alt="logo" className="logo" />
         <h1 className="title">Email has been sent!</h1>
-        <p className="subtitle">
-          Please check your inbox for instructions on how to reset the password
-        </p>
+        <p className="subtitle">Please check your inbox for instructions on how to reset the password</p>
         <div className="email-image">
           <img src="./icons/email.svg" alt="email" />
         </div>
@@ -21,6 +19,6 @@ const RecoveryPassword = () => {
       </div>
     </div>
   );
-};
+}
 
-export default RecoveryPassword;
+export default SendEmail;
