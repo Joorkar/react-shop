8a2960cc3d21ac8267a168ff791ccf03eccda3e0
Joorkar Romero <joorkarobrajham@gmail.com>
2021-11-07 19:15:25 -0600

    ADD funcionalidad al carrito de compras

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -17,7 +17,7 @@ const ProductItem = ({ product }) => {
       <img src={product.images[0]} alt={product.title} />
       <div className="product-info">
         <div>
-          <p>${product.price}.00</p>
+          <p>${product.price}</p>
           <p>{product.title}</p>
         </div>
         <figure onClick={() => handleClick(product)} >
--- src/hooks/useInitialState.js
+++ src/hooks/useInitialState.js
@@ -7,16 +7,36 @@ const initialState = {
 const useInitialState = () => {
   const [state, setState] = useState(initialState);
 
+  const verifyIdDoesNotExist = (id) => {
+    const idExist = state.cart.some(item => item.id === id);
+    if (idExist) {
+      return verifyIdDoesNotExist(id + 1);
+    } else {
+      return id;
+    }
+  };
+
   const addToCart = (payload) => {
+    const newId = verifyIdDoesNotExist(payload.id);
+    const payloadWithNewId = { ...payload, id: newId };
+
     setState({
       ...state,
-      cart: [...state.cart, payload],
+      cart: [...state.cart, payloadWithNewId],
+    });
+  }
+
+  const removeFromCart = (payload) => {
+    setState({
+      ...state,
+      cart: state.cart.filter((item) => item.id !== payload.id),
     });
   }
 
   return {
     state,
     addToCart,
+    removeFromCart,
   }
 };
 
