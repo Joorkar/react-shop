286dd48147e539f4ddc86a642fc4b7f03db50749
Joorkar Romero <joorkarobrajham@gmail.com>
2022-07-05 21:02:56 -0600

    issue 4 toggle addToCart and fix nav

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -2,12 +2,21 @@ import React, { useContext } from 'react';
 import '@styles/ProductItem.scss';
 import AppContext from '@context/AppContext';
 import addCart from '@icons/bt_add_to_cart.svg';
+import addedCart from '@icons/bt_added_to_cart.svg';
 
 const ProductItem = ({ product }) => {
-  const { addToCart } = useContext(AppContext);
+  const { addToCart, removeFromCart, state } = useContext(AppContext);
 
+  const itsProductAdded = () => {
+    return (state.cart.some(item => item.id === product.id)
+      ? true
+      : false
+    )
+  }
   const handleClick = item => {
-    addToCart(item);
+    itsProductAdded()
+      ? removeFromCart(item)
+      : addToCart(item);
   };
 
   return (
@@ -19,7 +28,10 @@ const ProductItem = ({ product }) => {
           <p>{product.title}</p>
         </div>
         <figure onClick={() => handleClick(product)} >
-          <img src={addCart} alt="button add cart" />
+          {itsProductAdded()
+            ? <img src={addedCart} alt="button added cart" />
+            : <img src={addCart} alt="button add cart" />
+          }
         </figure>
       </div>
     </div>
